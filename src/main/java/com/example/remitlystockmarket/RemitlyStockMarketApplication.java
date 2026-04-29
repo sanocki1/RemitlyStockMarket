1898608bca2ef96c6d858439999fcbbfb75ffe77
sanocki <hubert.sanocki2@gmail.com>
2026-04-29 19:58:33 +0200

    first commit with spring boot project initialized

---
+++ src/main/java/com/example/remitlystockmarket/RemitlyStockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.remitlystockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitlyStockMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemitlyStockMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/remitlystockmarket/RemitlyStockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remitlystockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitlyStockMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
